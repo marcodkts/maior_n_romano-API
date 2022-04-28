92a91ea6176e20c7f8d7d729058a83c5f2234a67
Marco Aurélio <marco.aurelio@a55.tech>
2022-04-28 18:30:40 -0300

    fix

--- controller/major_roman_number.py
+++ controller/major_roman_number.py
@@ -1,31 +1,25 @@
 def majorRomanNumber(text):
     nums = {"M": 1000, "D": 500, "C": 100, "L": 50, "X": 10, "V": 5, "I": 1}
     sum = 0
+    maior_valor = 0
     number = ""
-    sum_results = []
-    num_results = []
 
     for i in range(len(text)):
-        try:
-            value = nums[text[i]]
-            number += text[i]
-            if text[i + 1] not in nums:
-                sum += value
-            elif i + 1 < len(text) and nums[text[i + 1]] > value:
-                sum -= value
-            else:
-                sum += value
-            if i + 1 == len(text):
-                sum_results.append(sum)
-                num_results.append(number)
-        except ValueError:
-            sum_results.append(sum)
+        value = nums.get(text[i], 0)
+        if value == 0:
+            if sum > maior_valor:
+                maior_valor = sum
+                maior_numero = number
             sum = 0
-            num_results.append(number)
             number = ""
-
-    maior_valor = max(sum_results)
-    maior_numero = num_results[sum_results.index(maior_valor)]
+            continue
+        number += text[i] if text[i] in nums else ""
+        if i + 1 < len(text) and nums.get(text[i + 1], 0) > value:
+            sum -= value
+        else:
+            sum += value
+            if sum > maior_valor:
+                maior_valor = sum
+                maior_numero = number
 
     return {"number": maior_numero, "value": maior_valor}
-       
