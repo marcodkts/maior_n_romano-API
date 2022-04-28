1fe2734b71ef83c03c4bd9a5cf83bec861ebffcf
Marco Aurélio <marco.aurelio@a55.tech>
2022-04-28 17:36:32 -0300

    update

--- controller/major_roman_number.py
+++ controller/major_roman_number.py
@@ -1,21 +1,21 @@
-def majorRomanNumber(content):
+def majorRomanNumber(text):
     nums = {"M": 1000, "D": 500, "C": 100, "L": 50, "X": 10, "V": 5, "I": 1}
     sum = 0
     number = ""
     sum_results = []
     num_results = []
 
-    for i in range(len(content)):
+    for i in range(len(text)):
         try:
-            value = nums[content[i]]
-            number += content[i]
-            if content[i + 1] not in nums:
+            value = nums[text[i]]
+            number += text[i]
+            if text[i + 1] not in nums:
                 sum += value
-            elif i + 1 < len(content) and nums[content[i + 1]] > value:
+            elif i + 1 < len(text) and nums[text[i + 1]] > value:
                 sum -= value
             else:
                 sum += value
-            if i + 1 == len(content):
+            if i + 1 == len(text):
                 sum_results.append(sum)
                 num_results.append(number)
         except ValueError:
